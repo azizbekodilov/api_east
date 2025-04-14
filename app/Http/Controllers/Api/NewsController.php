91f948305f251073d297de8f9b1253455a77651c
Aziz <mu.azizbek@gmail.com>
2025-04-14 10:51:07 +0500

    update

--- app/Http/Controllers/Api/NewsController.php
+++ app/Http/Controllers/Api/NewsController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\News;
 use App\Models\NewsTranslation;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
 
 class NewsController extends Controller
 {
