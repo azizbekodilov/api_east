f72f7b11965724931b98ef91b2647548cfc6b381
Aziz <mu.azizbek@gmail.com>
2025-04-26 10:34:06 +0500

    гз

--- routes/api.php
+++ routes/api.php
@@ -30,8 +30,8 @@ Route::middleware([Localization::class])->prefix('{locale}')->group(function () 
         // bosh sahifa partners
         Route::get('/partners', [ApiPartnerController::class, 'index']);
         Route::get('/footer_catalog', [IndexController::class, 'footerCatalog']);
-        Route::get('/search/{$key}', [IndexController::class, 'search']);
     });
+    Route::get('/search/{search}', [IndexController::class, 'search']);
     // barcha kataloglar subkataloglar bilan
     Route::get("/all_catalogs", [CatalogController::class, 'allCatalogs']);
     // kataloglar ro'yhati
