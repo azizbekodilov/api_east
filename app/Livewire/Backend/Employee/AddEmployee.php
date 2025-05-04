0228af66d216eae225f145c0206e60d2a26eb49c
Aziz <mu.azizbek@gmail.com>
2025-05-04 18:49:11 +0500

    update

--- app/Livewire/Backend/Employee/AddEmployee.php
+++ app/Livewire/Backend/Employee/AddEmployee.php
@@ -30,10 +30,23 @@ class AddEmployee extends Component
         $this->locale = 'ru';
     }
 
+    public function translit($s) {
+        $s = (string) $s; // преобразуем в строковое значение
+        $s = trim($s); // убираем пробелы в начале и конце строки
+        $s = function_exists('mb_strtolower') ? mb_strtolower($s) : strtolower($s); // переводим строку в нижний регистр (иногда надо задать локаль)
+        $s = strtr($s, array('а'=>'a','б'=>'b','в'=>'v','г'=>'g','д'=>'d','е'=>'e','ё'=>'e','ж'=>'j','з'=>'z','и'=>'i','й'=>'y','к'=>'k','л'=>'l','м'=>'m','н'=>'n','о'=>'o','п'=>'p','р'=>'r','с'=>'s','т'=>'t','у'=>'u','ф'=>'f','х'=>'h','ц'=>'c','ч'=>'ch','ш'=>'sh','щ'=>'shch','ы'=>'y','э'=>'e','ю'=>'yu','я'=>'ya','ъ'=>'','ь'=>''));
+        return $s; // возвращаем результат
+      }
+
     public function save()
     {
         try {
-            $employee = Employee::create($this->validate());
+            $validate = $this->validate();
+            $path = $this->media->store(
+                'employees/'.$this->translit($this->title).$this->media->extension()
+            );
+            $validate['media'] = $path;
+            $employee = Employee::create($validate);
             EmployeeTranslation::create(
                 [
                     'title' => $this->title,
