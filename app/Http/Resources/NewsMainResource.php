4b9f7ba6946a0bc4184fd4c5a38146a7bfc910ab
Aziz <mu.azizbek@gmail.com>
2025-05-08 10:37:15 +0500

    update

--- app/Http/Resources/NewsMainResource.php
+++ app/Http/Resources/NewsMainResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Facades\URL;
@@ -22,12 +23,17 @@ class NewsMainResource extends JsonResource
             'text' => $this->short_text,
             'slug' => $this->slug,
             'locale' => $this->locale,
-            'created_at' => $this->created_at,
+            'created_at' => $this->created_at(),
         ];
     }
 
     public function media()
     {
         return URL::asset('/storage/news/'.$this->media);
+    }
+
+    public function created_at()
+    {
+        return Carbon::parse($this->created_at)->format('d.m.Y');
     }
 }
